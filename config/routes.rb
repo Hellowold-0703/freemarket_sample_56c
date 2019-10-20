693d3a88b31badbb6187b0be497c65f6d05b070d
Ayaki-Matsuki <super.crou@gmail.com>
2019-10-20 19:22:15 +0900

    Create top controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'products#index'
+  root 'top#index'
   resources :mypage, only: [:index]
-  resources :product, only: [:show]
+  resources :products, only: [:index]
 end

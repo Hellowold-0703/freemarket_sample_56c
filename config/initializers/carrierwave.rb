638afdf3f1a0ff586faa1940933dd77dc2c69b74
Helloworld-0703 <h.ryohei0703@gmail.com>
2019-10-22 14:21:10 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,7 +15,7 @@ CarrierWave.configure do |config|
     config.fog_directory  = 'freemarket56c'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket56c'
   else
-    config.storge :file
+    config.storage :file
     config.enable_processing = false if Rails.env.test?
   end
 end

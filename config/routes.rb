5138912687b991bea3c55c69e60a6e60fc4511d4
fsy218 <999fsy218@gmail.com>
2019-10-27 16:12:22 +0900

    商品購入、商品出品ページのルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,8 @@ Rails.application.routes.draw do
   resources :products, only: [:index]
   resources :creditcards, only: [:index]
   resources :users, only: [:signup]
-  resources :transaction do
-    collection do
-      get 'buy', 'sell'
-    end
+  namespace :transaction do
+    resources :buy, only: [:index]
+    resources :sell, only: [:index]
   end
 end

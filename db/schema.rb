c9caa26532edb3a0f56e50fd3620cf39a1676c89
bruno1412 <touho1507@gmail.com>
2019-10-27 15:40:10 +0900

    ユーザー確認画面更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191006012041) do
+ActiveRecord::Schema.define(version: 20191020015722) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false

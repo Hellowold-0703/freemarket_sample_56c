a9e4409bd4959c16aae040aad31edc65646a8ea5
Helloworld-0703 <h.ryohei0703@gmail.com>
2019-11-25 20:26:10 +0900

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,9 +30,8 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
     @seller = Seller.find_by(product_id: @product.id)
-    @sellers = Seller.where("user_id = ?", @seller.user_id).limit(6)
-    @products = Product.limit(6)
-    @nike_products = @products.where("brand_id = ?", @product.brand_id)
+    @sellers = Seller.where("user_id = ?", @seller.user_id).where.not("product_id = ?", @product.id).limit(6)
+    @nike_products = Product.where("brand_id = ?", @product.brand_id).limit(6)
   end
 
   private

aec93a361c5caf07df4331aae070e3ead1827dba
Ayaki-Matsuki <super.crou@gmail.com>
2019-12-01 14:09:09 +0900

    ビューの崩れんの修正

--- db/migrate/20191130150246_addnickname_tousers.rb
+++ db/migrate/20191130150246_addnickname_tousers.rb
@@ -1,6 +1,6 @@
 class AddnicknameTousers < ActiveRecord::Migration[5.0]
   def change     
-    add_column :users,:nickname,               :string
+    add_column :users,:nickname,           :string
     add_column :users,:birth_month,        :integer
     add_column :users,:birth_day,          :integer
 

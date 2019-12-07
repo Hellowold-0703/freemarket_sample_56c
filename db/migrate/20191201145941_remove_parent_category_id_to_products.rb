c069794f28ae0e816298b66050474c9121ff1a70
fsy218 <999fsy218@gmail.com>
2019-12-07 17:39:39 +0900

    マイグレーションファイルの整理

--- db/migrate/20191201145941_remove_parent_category_id_to_products.rb
+++ db/migrate/20191201145941_remove_parent_category_id_to_products.rb
@@ -1,5 +1,5 @@
 class RemoveParentCategoryIdToProducts < ActiveRecord::Migration[5.0]
   def change
-    remove_column :products, :parent_category_id
+    remove_column :products, :parent_category_id, :integer
   end
 end

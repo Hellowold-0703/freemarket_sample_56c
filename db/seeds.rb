6856425c51a49e71a99997a3d88f72375da0edfc
fsy218 <999fsy218@gmail.com>
2019-12-07 17:38:44 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,43 +1,3 @@
-ladies = Category.create(name: "レディース")
-
-tops = ladies.children.create(name: "トップス", size_type_id: 1)
-jacket_outer = ladies.children.create(name: "ジャケット/アウター", size_type_id: 1)
-pants = ladies.children.create(name: "パンツ", size_type_id: 1)
-
-tops.children.create([{name: "Tシャツ/カットソー(半袖/袖なし)"}, {name: "Tシャツ/カットソー(七分/長袖)"}, {name: "シャツ/ブラウス(半袖/袖なし)"}])
-jacket_outer.children.create([{name: "テーラージャケット"}, {name: "ノーカラージャケット"}, {name: "Gジャン/デニムジャケット"}])
-pants.children.create([{name: "デニム/ジーンズ"}, {name: "ショートパンツ"}, {name: "カジュアルパンツ"}])
-
-
-mens = Category.create(name: "メンズ")
-
-tops = mens.children.create(name: "トップス", size_type_id: 1)
-jacket_outer = mens.children.create(name: "ジャケット/アウター", size_type_id: 1)
-pants = mens.children.create(name: "パンツ", size_type_id: 1)
-
-tops.children.create([{name: "Tシャツ/カットソー(半袖/袖なし)"}, {name: "Tシャツ/カットソー(七分/長袖)"}, {name: "シャツ"}])
-jacket_outer.children.create([{name: "テーラージャケット"}, {name: "ノーカラージャケット"}, {name: "Gジャン/デニムジャケット"}])
-pants.children.create([{name: "デニム/ジーンズ"}, {name: " ワークパンツ/カーゴパンツ"}, {name: "スラックス"}])
-
-
-baby_kids = Category.create(name: "ベビー・キッズ")
-
-baby_girl = baby_kids.children.create(name: "ベビー服(女の子用) ~95cm", size_type_id: 3)
-baby_boy = baby_kids.children.create(name: "ベビー服(男の子用) ~95cm", size_type_id: 3)
-baby_unisex = baby_kids.children.create(name: "ベビー服(男女兼用) ~95cm", size_type_id: 3)
-
-baby_girl.children.create([{name: "トップス"}, {name: "アウター"}, {name: "パンツ"}])
-baby_boy.children.create([{name: "トップス"}, {name: "アウター"}, {name: "パンツ"}])
-baby_unisex.children.create([{name: "トップス"}, {name: "アウター"}, {name: "パンツ"}])
-
-
-
-chanel = Brand.create(name: "シャネル")
-nike = Brand.create(name: "ナイキ")
-louis_vuitton = Brand.create(name: "ルイ・ヴィトン")
-supreme = Brand.create(name: "シュプリーム")
-adidas = Brand.create(name: "アディダス")
-
 SizeType.create!(
   [
     {
@@ -158,3 +118,44 @@ Size.create!(
     },
   ]
 )
+
+ladies = Category.create!(name: "レディース")
+
+tops = ladies.children.create!(name: "トップス", size_type_id: 1)
+jacket_outer = ladies.children.create!(name: "ジャケット/アウター", size_type_id: 1)
+pants = ladies.children.create!(name: "パンツ", size_type_id: 1)
+
+tops.children.create!([{name: "Tシャツ/カットソー(半袖/袖なし)"}, {name: "Tシャツ/カットソー(七分/長袖)"}, {name: "シャツ/ブラウス(半袖/袖なし)"}])
+jacket_outer.children.create!([{name: "テーラージャケット"}, {name: "ノーカラージャケット"}, {name: "Gジャン/デニムジャケット"}])
+pants.children.create!([{name: "デニム/ジーンズ"}, {name: "ショートパンツ"}, {name: "カジュアルパンツ"}])
+
+
+mens = Category.create!(name: "メンズ")
+
+tops = mens.children.create!(name: "トップス", size_type_id: 1)
+jacket_outer = mens.children.create!(name: "ジャケット/アウター", size_type_id: 1)
+pants = mens.children.create!(name: "パンツ", size_type_id: 1)
+
+tops.children.create!([{name: "Tシャツ/カットソー(半袖/袖なし)"}, {name: "Tシャツ/カットソー(七分/長袖)"}, {name: "シャツ"}])
+jacket_outer.children.create!([{name: "テーラージャケット"}, {name: "ノーカラージャケット"}, {name: "Gジャン/デニムジャケット"}])
+pants.children.create!([{name: "デニム/ジーンズ"}, {name: " ワークパンツ/カーゴパンツ"}, {name: "スラックス"}])
+
+
+baby_kids = Category.create!(name: "ベビー・キッズ")
+
+baby_girl = baby_kids.children.create!(name: "ベビー服(女の子用) ~95cm", size_type_id: 3)
+baby_boy = baby_kids.children.create!(name: "ベビー服(男の子用) ~95cm", size_type_id: 3)
+baby_unisex = baby_kids.children.create!(name: "ベビー服(男女兼用) ~95cm", size_type_id: 3)
+
+baby_girl.children.create!([{name: "トップス"}, {name: "アウター"}, {name: "パンツ"}])
+baby_boy.children.create!([{name: "トップス"}, {name: "アウター"}, {name: "パンツ"}])
+baby_unisex.children.create!([{name: "トップス"}, {name: "アウター"}, {name: "パンツ"}])
+
+
+
+chanel = Brand.create!(name: "シャネル")
+nike = Brand.create!(name: "ナイキ")
+louis_vuitton = Brand.create!(name: "ルイ・ヴィトン")
+supreme = Brand.create!(name: "シュプリーム")
+adidas = Brand.create!(name: "アディダス")
+

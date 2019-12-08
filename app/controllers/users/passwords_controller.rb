332655cd7797cd6f0f5cb5fd17e5661f78b90092
Ayaki-Matsuki <super.crou@gmail.com>
2019-12-09 00:27:45 +0900

    recapchaを導入

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class Users::PasswordsController < Devise::PasswordsController
+  prepend_before_action :check_captcha, only: [:create]
+  private   def check_captcha
+    unless verify_recaptcha
+      self.resource = resource_class.new
+      resource.validate # Look for any other validation errors besides Recaptcha
+      respond_with_navigational(resource) { render :new }
+    end
+  end
   # GET /resource/password/new
   # def new
   #   super
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers:{ omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers:{ 
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root 'products#index'
   resources :mypage, only: [:index]

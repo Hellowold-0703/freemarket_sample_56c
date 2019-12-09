e9220652fb1361d1e679540d89e261b6f4af9fad
Ayaki-Matsuki <super.crou@gmail.com>
2019-12-09 16:45:30 +0900

    検索機能Lv１の編集

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
       get 'get_size', defaults: { format: 'json' }
-      get 'search'
+      get 'search', to: 'products#search'
     end
   end
   get "/transaction/sell", to: "products#new"

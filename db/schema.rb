14b1ad1e138c5e8277b949340176ad8c729d7ea7
Ayaki-Matsuki <super.crou@gmail.com>
2019-12-11 15:39:29 +0900

    コンフリクト修正

--- db/schema.rb
+++ db/schema.rb
@@ -126,8 +126,7 @@ ActiveRecord::Schema.define(version: 20191208064314) do
     t.string   "phone_number"
     t.string   "provider"
     t.string   "uid"
-    t.string   "nickname",                                              null: false
-    t.string   "last_name",                                             null: false
+    t.string   "nickname"
     t.integer  "birth_month"
     t.integer  "birth_day"
     t.string   "address_last_name"
